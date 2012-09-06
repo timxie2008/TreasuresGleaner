943e450aa3a88e2dfbc76f92bfb8f4045de9a8f2
David Han <umbrahan@gmail.com>
2012-09-06 10:23:20 +0800

    res rec

---
+++ Material/_update.cmd
@@ -0,0 +1,19 @@
+copy .\env\env.plist ..\Resources\env\ /Y
+copy .\env\env.png ..\Resources\env\ /Y
+
+copy .\blockers\blockers.plist ..\Resources\env\ /Y
+copy .\blockers\blockers.png ..\Resources\env\ /Y
+
+copy .\ui\ui.plist ..\Resources\ui\ /Y
+copy .\ui\ui.png ..\Resources\ui\ /Y
+
+copy .\dolphin\dolphin.plist ..\Resources\character\ /Y
+copy .\dolphin\dolphin.png ..\Resources\character\ /Y
+
+copy .\shark\shark.plist ..\Resources\character\ /Y
+copy .\shark\shark.png ..\Resources\character\ /Y
+
+copy .\player\player.plist ..\Resources\character\ /Y
+copy .\player\player.png ..\Resources\character\ /Y
+
+pause
