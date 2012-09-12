54cd2ddddae9d458b2ef8e6aa97093fc5ccef758
David Han <umbrahan@gmail.com>
2012-09-12 20:22:11 +0800

    android files

--- android/src/com/crazyamber/ttlus/MainActivity.java
+++ android/src/com/crazyamber/ttlus/MainActivity.java
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
  ****************************************************************************/
-package com.crazyamber.treasureshunter;
+package com.crazyamber.ttlus;
 
 import org.cocos2dx.lib.Cocos2dxActivity;
 import org.cocos2dx.lib.Cocos2dxEditText;
@@ -33,47 +33,76 @@ import android.content.Context;
 import android.content.pm.ConfigurationInfo;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
-public class TreasuresHunter extends Cocos2dxActivity {
+public class MainActivity extends Cocos2dxActivity {
 	private Cocos2dxGLSurfaceView mGLView;
-
-	private boolean detectOpenGLES20() {
-		ActivityManager am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-		ConfigurationInfo info = am.getDeviceConfigurationInfo();
-		return (info.reqGlEsVersion >= 0x20000);
-	}
-
-	protected void onCreate(Bundle savedInstanceState) {
+	
+	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-
+		
 		if (detectOpenGLES20()) {
 			// get the packageName,it's used to set the resource path
 			String packageName = getApplication().getPackageName();
 			super.setPackageName(packageName);
+			
+            // FrameLayout
+            ViewGroup.LayoutParams framelayout_params =
+                new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,
+                                           ViewGroup.LayoutParams.FILL_PARENT);
+            FrameLayout framelayout = new FrameLayout(this);
+            framelayout.setLayoutParams(framelayout_params);
 
-			setContentView(R.layout.game_frame);
-			mGLView = (Cocos2dxGLSurfaceView) findViewById(R.id.game_gl_surfaceview);
-			mGLView.setTextField((Cocos2dxEditText) findViewById(R.id.textField));
-			mGLView.setEGLContextClientVersion(2);
-			mGLView.setCocos2dxRenderer(new Cocos2dxRenderer());
-		} else {
+            // Cocos2dxEditText layout
+            ViewGroup.LayoutParams edittext_layout_params =
+                new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,
+                                           ViewGroup.LayoutParams.WRAP_CONTENT);
+            Cocos2dxEditText edittext = new Cocos2dxEditText(this);
+            edittext.setLayoutParams(edittext_layout_params);
+
+            // ...add to FrameLayout
+            framelayout.addView(edittext);
+
+            // Cocos2dxGLSurfaceView
+	        mGLView = new Cocos2dxGLSurfaceView(this);
+
+            // ...add to FrameLayout
+            framelayout.addView(mGLView);
+
+	        mGLView.setEGLContextClientVersion(2);
+	        mGLView.setCocos2dxRenderer(new Cocos2dxRenderer());
+            mGLView.setTextField(edittext);
+
+            // Set framelayout as the content view
+			setContentView(framelayout);
+		}
+		else {
 			Log.d("activity", "don't support gles2.0");
 			finish();
-		}
+		}	
 	}
+	
+	 @Override
+	 protected void onPause() {
+	     super.onPause();
+	     mGLView.onPause();
+	 }
 
-	@Override
-	protected void onPause() {
-		super.onPause();
-		mGLView.onPause();
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-		mGLView.onResume();
-	}
-
+	 @Override
+	 protected void onResume() {
+	     super.onResume();
+	     mGLView.onResume();
+	 }
+	 
+	 private boolean detectOpenGLES20() 
+	 {
+	     ActivityManager am =
+	            (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+	     ConfigurationInfo info = am.getDeviceConfigurationInfo();
+	     return (info.reqGlEsVersion >= 0x20000);
+	 }
+		
 	static {
 		System.loadLibrary("game_frame");
 	}
